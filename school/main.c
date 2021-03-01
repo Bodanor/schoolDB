2e737c51d8601428841eb5e0cfb30d39ea250aa3
Liwinux <papadopoulos.chris@icloud.com>
2021-03-02 00:04:40 +0100

    Initial Commit

---
+++ school/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  school
+//
+//  Created by Christos Papadopoulos on 02/03/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
